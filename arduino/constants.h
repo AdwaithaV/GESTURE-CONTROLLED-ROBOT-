6a429166d724dc5c0841bf304e965213ad5f728e
ADWAITHA V <142923950+AdwaithaV@users.noreply.github.com>
2025-11-02 16:38:40 +0530

    Update constants.h

--- arduino/constants.h
+++ arduino/constants.h
@@ -1,1 +1,26 @@
+//MOTOR PINS
+
+#define ENA 5    // Left Motor Enable (PWM)
+#define IN1 8
+#define IN2 9
+
+#define ENB 6    // Right Motor Enable (PWM)
+#define IN3 10
+#define IN4 11
+
+// ULTRASONIC PINS
+
+#define TRIG_PIN 2
+#define ECHO_PIN 3
+
+// SPEED CONFIG 
+
+#define MAX_SPEED 255        // Full speed
+#define TURN_SPEED 180       // Speed when turning
+#define ACCEL_STEP 15        // How quickly speed increases per command
+#define FIXED_AUTO_SPEED 200 // Constant speed in Auto mode
+
+//  OBSTACLE SETTINGS
+
+#define SAFE_DISTANCE 25     // cm
 
